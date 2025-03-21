5cf3a97a79ae65ba1df2f7444e590a4dfe699cc2
jabaltariq <21aidanrodriguez1@gmail.com>
2025-03-21 17:02:37 -0400

    Initial commit of all the classes.
    These classes invoice:
    Invoice.java - Class for the invoices, to be populated by the database.
    InvoiceViewController.java - Class to control invoice-view.fxml
    Item.java - Class for the items, to be populated by the database.
    Status.java - Enum for the invoice Statuses.
    invoice_view.fxml - FXML page for the invoice views.

---
+++ src/main/java/com/example/csc311_capstone_project/Invoice.java
@@ -0,0 +1,10 @@
+package com.example.csc311_capstone_project;
+
+public class Invoice {
+    public int invoice_id;
+    public int account_id;
+    public String delivery_address;
+    public Status status;
+
+
+}
---
+++ src/main/java/com/example/csc311_capstone_project/Status.java
@@ -0,0 +1,7 @@
+package com.example.csc311_capstone_project;
+
+public enum Status {
+    delivered,
+    en_route,
+    not_delivered
+}
