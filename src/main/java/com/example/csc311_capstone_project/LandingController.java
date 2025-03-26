5a070dd8f932b59c14ced5dc6452f812c59ebd9b
Riiel-edu <riven18@farmingdale.edu>
2025-03-26 16:44:51 -0400

    Scanner page is completely ready for implementation of the API, the list cannot be added to the Landing page as the Invoice item doesn't exist in the code I'm using

--- src/main/java/com/example/csc311_capstone_project/LandingController.java
+++ src/main/java/com/example/csc311_capstone_project/LandingController.java
@@ -2,7 +2,11 @@ package com.example.csc311_capstone_project;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class LandingController {
-
 }
