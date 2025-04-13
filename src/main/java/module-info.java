9eb3dd0f9191d9d32cc1cfc54146eaa862c20268
Riiel-edu <riven18@farmingdale.edu>
2025-04-13 14:45:10 -0400

    DB shell code now added to the project for implementation

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.csc311_capstone_project {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.csc311_capstone_project to javafx.fxml;
