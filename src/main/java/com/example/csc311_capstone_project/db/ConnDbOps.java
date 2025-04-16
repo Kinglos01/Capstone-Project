7cd64ddac63bd3cc46d62664a43d9618048bb680
Riiel-edu <riven18@farmingdale.edu>
2025-04-16 14:44:37 -0400

    insert into statements

--- src/main/java/com/example/csc311_capstone_project/db/ConnDbOps.java
+++ src/main/java/com/example/csc311_capstone_project/db/ConnDbOps.java
@@ -37,7 +37,7 @@ public class ConnDbOps {
             //Second, connect to the database and create the table "users" if cot created
             conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
             statement = conn.createStatement();
-            String sql = "CREATE TABLE IF NOT EXISTS users ("
+            String sql = "CREATE TABLE IF NOT EXISTS user ("
                     + "id INT( 10 ) NOT NULL PRIMARY KEY AUTO_INCREMENT,"
                     + "name VARCHAR(200) NOT NULL,"
                     + "email VARCHAR(200) NOT NULL UNIQUE,"
@@ -73,7 +73,7 @@ public class ConnDbOps {
 
         try {
             Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
-            String sql = "SELECT * FROM users WHERE name = ?";
+            String sql = "SELECT * FROM user WHERE name = ?";
             PreparedStatement preparedStatement = conn.prepareStatement(sql);
             preparedStatement.setString(1, name);
 
@@ -100,7 +100,7 @@ public class ConnDbOps {
 
         try {
             Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
-            String sql = "SELECT * FROM users ";
+            String sql = "SELECT * FROM user ";
             PreparedStatement preparedStatement = conn.prepareStatement(sql);
 
             ResultSet resultSet = preparedStatement.executeQuery();
@@ -121,18 +121,26 @@ public class ConnDbOps {
         }
     }
 
-    public  void insertUser(String name, String email, String phone, String address, String password) {
-
+    /**
+     * Adds a new user into the SQL table user. The PK consists of username and email,
+     * both of which must be unique and NOT NULL.
+     * @param username The username of the user must be unique.
+     * @param email The email of the user must be unique.
+     * @param password The password of the user.
+     * @param first_name The first_name of the user.
+     * @param last_name The last_name of the user.
+     */
+    public void insertUser(String username, String email, String password, String first_name, String last_name) {
 
         try {
             Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
-            String sql = "INSERT INTO users (name, email, phone, address, password) VALUES (?, ?, ?, ?, ?)";
+            String sql = "INSERT INTO user (username, email, password, first_name, last_name) VALUES (?, ?, ?, ?, ?)";
             PreparedStatement preparedStatement = conn.prepareStatement(sql);
-            preparedStatement.setString(1, name);
+            preparedStatement.setString(1, username);
             preparedStatement.setString(2, email);
-            preparedStatement.setString(3, phone);
-            preparedStatement.setString(4, address);
-            preparedStatement.setString(5, password);
+            preparedStatement.setString(3, password);
+            preparedStatement.setString(4, first_name);
+            preparedStatement.setString(5, last_name);
 
             int row = preparedStatement.executeUpdate();
 
@@ -146,6 +154,44 @@ public class ConnDbOps {
             e.printStackTrace();
         }
     }
+
+    /**
+     * Adds a new invoice into the SQL Table invoice. The PK consists of invoice_id, username and email.
+     * Username and Email are both foreign keys from the user table.
+     * @param invoice_id The id for the current invoice must be unique for the current user.
+     * @param username The username for the user, must match one of the usernames in the user table.
+     * @param email The current email for the user, must match one of the emails in the user table.
+     * @param order_date The date the invoice was ordered.
+     * @param delivery_date The day the invoice was delivered if applicable.
+     * @param status The current status of the invoice.
+     */
+    public void insertInvoice(String invoice_id, String username, String email, String order_date , String delivery_date, String status) {
+
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+            String sql = "INSERT INTO invoice (invoice_id, username, email, order_date, delivery_date, status) VALUES (?, ?, ?, ?, ?, ?)";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1, invoice_id);
+            preparedStatement.setString(2, username);
+            preparedStatement.setString(3, email);
+            preparedStatement.setString(4, order_date);
+            preparedStatement.setString(5, delivery_date);
+            preparedStatement.setString(6, status);
+
+            int row = preparedStatement.executeUpdate();
+
+            if (row > 0) {
+                System.out.println("A new invoice was inserted successfully.");
+            }
+
+            preparedStatement.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    
 
 
 }
