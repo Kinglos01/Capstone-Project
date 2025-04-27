557336f5d7fd83fd99561880782d5c3f5ed5b21b
Riiel-edu <riven18@farmingdale.edu>
2025-04-27 17:35:54 -0400

    Added shell for the menu items and the layout for the item page

---
+++ src/main/java/com/example/csc311_capstone_project/ItemController.java
@@ -0,0 +1,32 @@
+package com.example.csc311_capstone_project;
+
+import javafx.scene.input.MouseEvent;
+
+public class ItemController {
+
+    /**
+     * Changes the image of the invoice displayed on the right with the one of the selected invoice.
+     * @param mouseEvent The event of clicking on a specific TableView value.
+     * @since 4/23/2025
+     * @author Nathaniel Rivera
+     */
+    public void selectedInvoice(MouseEvent mouseEvent) {
+        /*
+        try {
+            Invoice invoice = null;
+
+            for (Invoice value : invoices) {
+                if (Objects.equals(value.getInvoiceId(), invoiceTable.getSelectionModel().getSelectedItem().getInvoiceId())) {
+                    invoice = value;
+                }
+            }
+
+
+            invoiceDisplay.setImage(Objects.requireNonNull(invoice).getImage());
+        } catch(NullPointerException _) { }*/
+    }
+
+    public void delete() {
+
+    }
+}
