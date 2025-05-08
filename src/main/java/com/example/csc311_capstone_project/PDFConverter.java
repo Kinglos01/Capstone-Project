60fa4011d1510336b1f303061c565d072a3d2c48
Nathaniel Rivera <riven18@farmingdale.edu>
2025-05-07 20:28:40 -0400

    Fixed files with spaces to be able to scanned by the PDF scanner

--- src/main/java/com/example/csc311_capstone_project/PDFConverter.java
+++ src/main/java/com/example/csc311_capstone_project/PDFConverter.java
@@ -26,6 +26,9 @@ public class PDFConverter {
         String pdfFilePath = filepath.substring(5);
         String outputPngPrefix = filepath.substring(5, filepath.length() - 4);
         File pngFile = null;
+
+        pdfFilePath = pdfFilePath.replaceAll("%20", " ");
+
         System.out.println("File path: " +  pdfFilePath);
 
         try {
