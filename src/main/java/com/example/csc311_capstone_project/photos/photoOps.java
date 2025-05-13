4cbb5c742a6652d2e3c7ddf605785cae116b5533
Anthony <costa18@farmingdale.edu>
2025-05-13 01:49:09 -0400

    added photoDB integration.

--- src/main/java/com/example/csc311_capstone_project/photos/photoOps.java
+++ src/main/java/com/example/csc311_capstone_project/photos/photoOps.java
@@ -8,12 +8,11 @@ import com.azure.storage.blob.models.BlobErrorCode;
 import com.azure.storage.blob.models.BlobStorageException;
 import javafx.scene.image.Image;
 
-import java.io.File;
-import java.io.UncheckedIOException;
+import java.io.*;
 
 public class photoOps {
-    final static String sasToken = "sas token";
-    final static String endpoint = "endpoint";
+    final static String sasToken = "sv=2024-11-04&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2025-05-31T13:07:43Z&st=2025-05-13T05:07:43Z&spr=https&sig=gzT5%2BNBEWQtfjJE%2FtnFuKEu0X9PWLCUcP7ilzwO2Fyc%3D";
+    final static String endpoint = "https://311invoicestorage.blob.core.windows.net/";
     protected static BlobServiceClient blobServiceClient;
     protected static BlobContainerClient containerClient;
     public static void connectToClient() {
@@ -39,4 +38,17 @@ public class photoOps {
             System.out.println("Failed to upload image file to DB");
         }
     }
+
+    public static InputStream getImageStream(String blobName) {
+        BlobClient blobClient = containerClient.getBlobClient(blobName);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try {
+            blobClient.download(outputStream);
+            return new ByteArrayInputStream(outputStream.toByteArray());
+        } catch (BlobStorageException e) {
+            System.out.println("Failed to load image from blob: " + e.getMessage());
+            return null;
+        }
+    }
+
 }
